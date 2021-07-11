e4a4fb71ffe1f8ebf5f9a89ff8639e923e00ffba
eliabe <eliabe.fc@gmail.com>
2021-07-11 00:21:22 -0300

    feat: add .watch command

---
+++ src/bot/commands/command-list/watch.ts
@@ -0,0 +1,30 @@
+import { Command, CommandData } from '../protocols/command';
+const yts = require('yt-search');
+
+const func: Command = async (params) => {
+  const { value, client, message } = params;
+
+  const videos = await yts(value).catch(() => false);
+
+  if (!videos || !videos.all || !videos.all[0]) {
+    await client.sendText(
+      message.from,
+      'Não foi possível encontrar o vídeo [' + value + ']'
+    );
+    return;
+  }
+
+  await client.sendYoutubeLink(
+    message.from,
+    videos.all[0].url,
+    `Resultado da pesquisa para "${value}"`
+  );
+};
+
+const watch: CommandData = {
+  command: '.watch',
+  description: 'Pesquisa um vídeo no youtube',
+  func,
+};
+
+export default watch;
