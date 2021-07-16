d52433b04f66b962c7808562be9eb8e3d4629a0a
Luan Costa <lcosta@wiley.com>
2021-07-16 11:27:50 -0300

    Adding manual group blocking

--- src/database/json/db.ts
+++ src/database/json/db.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 import { JsonDb } from './json-db';
 import { BlockedUsers } from '../models/blocked-users';
+import { BlockedGroups } from '../models/blocked-groups';
 import { History } from '../models/history';
 import { User } from '../models/user';
 import { Group } from '../models/groups';
@@ -12,10 +13,12 @@ const historyDbJson = path.join(dbFolder, 'history.json');
 const usersDbJson = path.join(dbFolder, 'users.json');
 const groupsDbJson = path.join(dbFolder, 'groups.json');
 const blockedUsersDbJson = path.join(dbFolder, 'blocked-users.json');
+const blockedGroupsDbJson = path.join(dbFolder, 'blocked-groups.json');
 const userStatsDbJson = path.join(dbFolder, 'user-stats.json');
 
 export const usersDb = new JsonDb<User>(usersDbJson);
 export const blockedUsersDb = new JsonDb<BlockedUsers>(blockedUsersDbJson);
+export const blockedGroupsDb = new JsonDb<BlockedGroups>(blockedGroupsDbJson);
 export const historyDb = new JsonDb<History>(historyDbJson);
 export const groupsDb = new JsonDb<Group>(groupsDbJson);
 export const userStatsDb = new JsonDb<UserStats>(userStatsDbJson);
---
+++ src/database/models/blocked-groups.ts
@@ -0,0 +1,6 @@
+export interface BlockedGroups {
+  groupId: string;
+  unblockDate: string;
+  created_at: string;
+  updated_at: string;
+}
