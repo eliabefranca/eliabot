5761e2e1b9b0766db520d45a8e670a400edfa376
eliabe <eliabe.fc@gmail.com>
2021-07-17 01:07:48 -0300

    feat: add delete and put routes for user in api

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-import { usersDb } from '../../database/json/db';
+import { usersDb } from "../../database/json/db";
 
 class UserController {
   get(req: Request, res: Response) {
@@ -8,7 +8,7 @@ class UserController {
     const user = usersDb.getFirst({ id });
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     return res.json(user);
@@ -18,6 +18,32 @@ class UserController {
     const users = usersDb.getData();
 
     return res.json(users);
+  }
+
+  delete(req: Request, res: Response) {
+    const { id } = req.params;
+    const user = usersDb.getFirst({ id });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    usersDb.delete(user);
+
+    return res.json(user);
+  }
+
+  put(req: Request, res: Response) {
+    const { id } = req.params;
+    const user = usersDb.getFirst({ id });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    usersDb.update(user, req.body);
+
+    return res.json(user);
   }
 }
 
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -6,6 +6,8 @@ const routes = Router();
 
 routes.get("/users/:id", UserController.get);
 routes.get("/users", UserController.getAll);
+routes.delete("/users/:id", UserController.delete);
+routes.put("/users/:id", UserController.put);
 
 routes.get("/groups", GroupController.getAll);
 routes.get("/groups/:id", GroupController.get);
