8e16bb53a4118338f540b74d89e379b3bc73ae78
Agronault <xande.luis@hotmail.com>
2021-07-17 12:45:29 -0300

    feat(commands): adds imitar3 tts command

---
+++ src/bot/commands/command-list/imitar3.ts
@@ -0,0 +1,39 @@
+import { Command, CommandData } from '../protocols/command';
+import * as googleTTS from 'google-tts-api';
+
+const func: Command = async ({ client, message, value }) => {
+  const { quotedMsg } = message;
+
+  if (!quotedMsg) {
+    await client.reply(
+      message.from,
+      'Você precisa responder a mensagem que deseja que eu imite.',
+      message.id
+    );
+    return;
+  }
+
+  const { body, caption } = quotedMsg;
+  let txt = quotedMsg.isMedia ? caption : body;
+
+  if (!txt.trim()) {
+    await client.reply(message.from, 'A mensagem está vazia.', message.id);
+    return;
+  }
+
+
+  const audioUrl = googleTTS.getAudioUrl(txt, {
+    lang: 'pt-BR',
+    host: 'https://translate.google.com',
+  });
+  client.sendAudio(message.chatId, audioUrl);
+};
+
+const imitar: CommandData = {
+  func,
+  command: '.imitar3',
+  description: 'Imita em audio uma mensagem marcada.',
+  onlyForGroups: false,
+};
+
+export default imitar;
