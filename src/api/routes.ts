d0da4bfcb7dd04b024e4849aabccbda89a65026f
eliabe <eliabe.fc@gmail.com>
2021-07-17 00:57:44 -0300

    refactor: remove history controller

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,16 +1,13 @@
-import { Router } from 'express';
-import HistoryController from './controllers/history';
-import UserController from './controllers/user';
-import GroupController from './controllers/group';
+import { Router } from "express";
+import UserController from "./controllers/user";
+import GroupController from "./controllers/group";
 
 const routes = Router();
 
-routes.get('/history', HistoryController.getAll);
+routes.get("/users/:id", UserController.get);
+routes.get("/users", UserController.getAll);
 
-routes.get('/users/:id', UserController.get);
-routes.get('/users', UserController.getAll);
-
-routes.get('/groups', GroupController.getAll);
-routes.get('/groups/:id', GroupController.get);
+routes.get("/groups", GroupController.getAll);
+routes.get("/groups/:id", GroupController.get);
 
 export default routes;
