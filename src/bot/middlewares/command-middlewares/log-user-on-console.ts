1ec1eb16e5a2df201c12cd37088c67d179a91e3b
eliabe <eliabe.fc@gmail.com>
2021-07-22 23:41:09 -0300

    feat: add logUserOnConsole middleware

--- src/bot/middlewares/command-middlewares/log-user-on-console.ts
+++ src/bot/middlewares/command-middlewares/log-user-on-console.ts
@@ -1,3 +1,4 @@
+import { getTimeStamp } from 'src/helpers/date';
 import { CommandMiddleware } from '../../bot';
 
 export const logUserOnConsole: CommandMiddleware = async ({
@@ -7,7 +8,9 @@ export const logUserOnConsole: CommandMiddleware = async ({
   query,
 }): Promise<boolean> => {
   console.log(
-    `Running command  ${commandData.command} for ${message.sender.pushname}`
+    `${getTimeStamp()}: Running command ${commandData.command} for ${
+      message.sender.pushname
+    }`
   );
 
   return true;
