13f56c46ccc5acf8e66474b801dfc5ba21b1521e
eliabe <eliabe.fc@gmail.com>
2022-07-30 16:47:00 -0300

    add video downloads folder to config

--- config.ts
+++ config.ts
@@ -4,4 +4,5 @@ export const CONFIG = {
   dbFolder: path.resolve(__dirname, 'db'),
   imageDownloadsFolder: path.resolve(__dirname, 'files', 'images'),
   screenshotsFolder: path.resolve(__dirname, 'files', 'screenshots'),
+  videoDownloadsFolder: path.resolve(__dirname, 'files', 'videos'),
 };
