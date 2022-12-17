7c9e5d4c67c551fa9c5e5110bafb0fc17a11c7a9
eliabe <eliabe.fc@gmail.com>
2022-12-17 00:20:05 -0300

    remove number restriction

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ async function main() {
   await bot.start();
 
   bot.on<Message<any>>('message', async (message) => {
-    if (!message.sender.id.includes('5512997246234')) {
-      return;
-    }
-
     const { keyword, value } = await parseCommand(message.text);
     const command = bot.commands.find((command) =>
       command.keywords.includes(keyword)
