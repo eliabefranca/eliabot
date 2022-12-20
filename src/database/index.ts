4cb3a057024c59ff923889862781897e2dadc80b
eliabe <eliabe.fc@gmail.com>
2022-12-20 00:16:39 -0300

    adjust in-memory db config for testing

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,15 +3,18 @@ import { Sequelize } from 'sequelize';
 const config = require('./config.json');
 
 let databaseCfg = config.production;
-if (process.env.NODE_ENV === 'test') {
-  databaseCfg = config.test;
-} else if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   databaseCfg = config.development;
 }
 
 databaseCfg.storage = path.join(__dirname, '..', '..', databaseCfg.storage);
 
-export const database = new Sequelize(databaseCfg);
+export const database =
+  process.env.NODE_ENV === 'test'
+    ? new Sequelize('sqlite::memory:', config.test)
+    : new Sequelize(databaseCfg);
 
-console.log('Database connected on mode', process.env.NODE_ENV);
-console.log('Database path', databaseCfg.storage);
+if (process.env.NODE_ENV !== 'test') {
+  console.log('Database connected on mode', process.env.NODE_ENV);
+  console.log('Database path', databaseCfg.storage);
+}
